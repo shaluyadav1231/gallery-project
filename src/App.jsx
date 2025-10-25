f04bc600db6d84029ce7fe4b99b855dc3f15b2fc
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-10-25 13:36:58 +0530

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
@@ -16,21 +17,14 @@ const App = () => {
     GetData();
   }, [index]);
 
-  let printUserData = "No User Available";
+  let printUserData = "Loading";
   if (printUserData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return (
         <div key={idx}>
-          <a href={elem.url} target="_blank">
-            <div className="h-40 w-44 overflow-hidden bg-white rounded-xl">
-              <img
-                className="h-full w-full object-cover"
-                src={elem.download_url}
-                alt=""
-              />
-            </div>
-            <h2 className="font-bold text-lg">{elem.author}</h2>
-          </a>
+
+          <Card elem={elem}/>
+         
         </div>
       );
     });
@@ -38,22 +32,26 @@ const App = () => {
   return (
     <div className="bg-black h-screen overflow-auto p-4 text-white">
       <h1 className="fixed bg-red-500 text-6xl">{index}</h1>
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+      <div className="flex  flex-wrap gap-4">{printUserData}</div>
 
       <div className="flex justify-center gap-6 items-center p-4">
         <button
+        style={{opacity:index==1?0.6:1}}
         onClick={()=>{
           if (index>1){
              setIndex(index-1)
+             setUserData([])
           }
             
         }}
         className="bg-amber-500 text-black rounded px-4 py-2 text-sm cursor-pointer active:scale95 font-semibold">
           Prev
         </button>
+        <h4>Page {index}</h4>
         <button 
         onClick={()=>{
             setIndex(index+1)
+              setUserData([])
         }}
         className="bg-amber-500 text-black rounded px-4 py-2 text-sm cursor-pointer active:scale95 font-semibold">
           Next
