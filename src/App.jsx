a155c262f43c19dc5972eb15a2dc9182c4c8e5fb
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-10-25 11:55:12 +0530

    setting API images with the help of axios and lorem picsum

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const App = () => {
-  const GetData =()=>{
-    console.log("data aa gya")
+
+const [userData, setUserData] = useState([])
+
+  const GetData =async()=>{
+    const responce = await axios.get("https://picsum.photos/v2/list?page=2&limit=30")
+
+    setUserData(responce.data)
+    console.log(responce.data)
+  }
+
+  let printUserData="No User Available"
+
+  if(printUserData.length>0){
+    printUserData=userData.map(function(elem,idx){
+      return <img src={elem.download_url} alt="" />
+    })
   }
   return (
     <div 
     onClick={GetData}
-    className='bg-black h-screen p-4 text-white'>
+    className='bg-black h-screen overflow-auto p-4 text-white'>
       <button className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white '>GetData</button>
+
+   <div>
+    {
+      printUserData
+    }
+   </div>
+
     </div>
   )
 }
