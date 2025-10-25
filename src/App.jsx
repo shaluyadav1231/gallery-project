f16873bcdc7ae027221500ee2720d0ffd3fce8c5
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-10-25 10:55:06 +0530

    setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const GetData =()=>{
+    console.log("data aa gya")
+  }
   return (
-    <div className='border-8'>App</div>
+    <div 
+    onClick={GetData}
+    className='bg-black h-screen p-4 text-white'>
+      <button className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white '>GetData</button>
+    </div>
   )
 }
 
